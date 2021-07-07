8b21d483aafa59e2f86930048b87eafba136fe29
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2021-07-07 12:09:05 +0300

    Updates

--- static/js/main.1c974bf5.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-01-components"]=this["webpackJsonpgoit-react-hw-01-components"]||[]).push([[0],[function(o,n){console.log("Hello")}],[[0,1]]]);
-//# sourceMappingURL=main.1c974bf5.chunk.js.map
