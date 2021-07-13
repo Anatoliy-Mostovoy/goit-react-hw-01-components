ba84d80ef7c3dfbf25899b6c25c8050b21f0ee60
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2021-07-13 13:03:34 +0300

    Done random color

--- src/Components/Statistics/StatsItem.js
+++ src/Components/Statistics/StatsItem.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { renderColor, renderRGBColor } from '../../helpers/randomColor';
 import s from './Statistics.module.css';
 
 export const StatsItem = ({ label, percentage }) => (
-  <li className={s.item}>
+  <li
+    className={s.item}
+    style={{ backgroundColor: renderRGBColor(renderColor) }}
+  >
     <span className={s.label}>{label}</span>
     <span className="percentage">{percentage}%</span>
   </li>
